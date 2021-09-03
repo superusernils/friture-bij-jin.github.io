176d469edaa76e750fb7c24e20ab9466beedccdb
superusernils <nilsdekoelekikker@hotmail.com>
2021-09-03 20:01:50 +0200

    updateje

--- js/script.js
+++ js/script.js
@@ -53,3 +53,32 @@ $(document).ready(function(){
     });
 
 });
+
+var $box = $('.box');
+
+  $('.closei').each(function(){
+    var color = $(this).css('backgroundColor');
+    var content = $(this).html();
+    $(this).click(function() {
+    $box.css('backgroundColor', color);
+    $box.addClass('open');
+    $box.find('p').html(content);
+  });
+    
+  $('.close').click(function() {
+    $box.removeClass('open');
+    $box.css('backgroundColor', 'transparent');
+    
+ });
+  
+  $('body').toggleClass('overlay');
+$("#pop-toggle").click(function(){
+  $(".popup").toggle();
+  $('body').toggleClass('overlay');
+})
+$(".close").click(function(){
+  $(".popup").toggle();
+  $('body').toggleClass('overlay');
+});
+  
+});
